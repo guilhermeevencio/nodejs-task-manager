4b9e4e468686a9557b6d7f795a916f7fb336fa0c
Guilherme <guilhermeevencioluz@gmail.com>
2022-05-11 20:57:02 -0300

    feat: add user

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  const usernameAlreadyExists = users.some((user) => user.username === username );
+  if(usernameAlreadyExists) {
+    return response.status(400).json({ error: 'username already exists!' })
+  }
+  const userData = { id: uuidv4(), name, username, todos: [] }
+  users.push(userData);
+  return response.status(201).json(userData);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
@@ -37,5 +44,7 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
   // Complete aqui
 });
+
+app.listen(3333);
 
 module.exports = app;
