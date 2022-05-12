e302314746ca41740f7cd3233352107a65cce786
Guilherme <guilhermeevencioluz@gmail.com>
2022-05-12 10:24:16 -0300

    feat: add the method to save  a todo.

--- src/index.js
+++ src/index.js
@@ -37,7 +37,19 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { deadline, title } = request.body;
+  const { user } = request;
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  };
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
